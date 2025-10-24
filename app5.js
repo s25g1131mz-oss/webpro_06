a21fefdb20d6d66b6e084160a63ff02b043d859f
Kouki Yamada <s25g1131mz@s.chibakoudai.jp>
2025-10-24 13:36:58 +0900

    jankenradio.ejsとjankenradio.jsを追加

--- app5.js
+++ app5.js
@@ -64,3 +64,32 @@ app.get("/janken", (req, res) => {
 });
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
+
+app.get("/jankenradio", (req, res) => {
+  const value = req.query.radio;
+  let win = Number( req.query.win );
+  let total = Number( req.query.total );
+  console.log( {hand, win, total});
+  const num = Math.floor( Math.random() * 3 + 1 );
+  let cpu = '';
+  let judgement = '';
+  if( num==1 ) cpu = 'グー';
+  else if( num==2 ) cpu = 'チョキ';
+  else cpu = 'パー';
+  // ここに勝敗の判定を入れる
+  // 以下の数行は人間の勝ちの場合の処理なので，
+  // 判定に沿ってあいこと負けの処理を追加する
+  judgement = '勝ち';
+  win += 1;
+  total += 1;
+  const display = {
+    your: hand,
+    cpu: cpu,
+    judgement: judgement,
+    win: win,
+    total: total
+  }
+  res.render( 'jankenradio', display );
+});
+
+app.listen(8000, () => console.log("Example app listening on port 8000!"));
---
+++ jankenradio.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/jankenradio", (req, res) => {
+
+  const value = req.query.radio; 
+  let win = Number(req.query.win)||0;
+  let total = Number(req.query.total)||0;
+
+  let hand = ''; 
+  if (value == '1') hand = 'ぐー';
+  else if (value == '2') hand = 'ちょき';
+  else hand = 'ぱー';
+
+  const num = Math.floor(Math.random() * 3 + 1);
+  let cpu = '';
+  if (num == 1) cpu = 'ぐー';
+  else if (num == 2) cpu = 'ちょき';
+  else cpu = 'ぱー';
+
+  let judgement = '';
+  total += 1;
+
+  if (hand == cpu) {
+    judgement = 'あいこ';
+  } else if (
+    (hand == 'グー' && cpu == 'チョキ') ||
+    (hand == 'チョキ' && cpu == 'パー') ||
+    (hand == 'パー' && cpu == 'グー')
+  ) {
+    judgement = '勝ち';
+    win += 1;
+  } else {
+    judgement = '負け';
+  }
+
+  console.log({ hand, cpu, judgement, win, total });
+
+  const display = {
+    your: hand,
+    cpu: cpu,
+    judgement: judgement,
+    win: win,
+    total: total
+  };
+
+  res.render('jankenradio', display);
+});
+
+app.listen(8000, () => console.log("Example app listening on port 8000!"));
